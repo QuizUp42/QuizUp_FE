fe5f3c6901daa3285569556455ebc0eb7f82fc96
정윤석 <mar0722@naver.com>
2025-05-17 19:35:37 +0900

    feat: 로그인, 회원가입 페이지 생성 및 라우터 연결

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,18 +1,21 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import RootLayout from "@/layouts/RootLayout";
-import MobileLayout from "@/layouts/MobileLayout";
 import Login from "../pages/Login";
 import MobileLogin from "../pages/mobile/Login";
+import AppLayout from "../components/layout/AppLayout";
+import Signup from "../pages/Signup";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
-    children: [{ path: "", element: <Login /> }],
+    children: [
+      { path: "", element: <Login /> },
+      { path: "/signup", element: <Signup /> },
+    ],
   },
   {
     path: "/mobile",
-    element: <MobileLayout />,
-    children: [{ path: "enter", element: <MobileLogin /> }],
+    element: <MobileLogin />,
+    // children: [{ path: "enter", element: <MobileLogin /> }],
   },
 ]);
