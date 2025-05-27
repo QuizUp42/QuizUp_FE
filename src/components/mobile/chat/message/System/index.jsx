7fe6c8b7eff9845a350845a7a61246f65cb51c9c
정윤석 <mar0722@naver.com>
2025-05-27 18:18:17 +0900

    feat: 채팅 기능 구현 및 테스트

---
+++ src/components/mobile/chat/message/System/index.jsx
@@ -0,0 +1,5 @@
+const MessageSystem = () => {
+  return <div>메세지 이미지</div>;
+};
+
+export default MessageSystem;
---
+++ src/components/mobile/chat/message/TeacherText/index.jsx
@@ -0,0 +1,5 @@
+const MessageTeacherText = () => {
+  return <div>메세지 이미지</div>;
+};
+
+export default MessageTeacherText;
