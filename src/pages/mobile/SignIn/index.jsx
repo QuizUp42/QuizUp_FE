f5bc7b9984abc79a87a5b9683e2b376c3e81f762
정윤석 <mar0722@naver.com>
2025-05-29 21:39:29 +0900

    feat: 닉네임 설정 페이지 UI 구현 및 스타일링

--- src/pages/mobile/SignIn/index.jsx
+++ src/pages/mobile/SignIn/index.jsx
@@ -9,8 +9,8 @@ const MobileSignIn = () => {
         placeholder="학번 / 교번 입력"
         className="w-full px-6 py-3 bg-primary-white text-black font-bold placeholder:text-[#b6b6b6] placeholder:font-bold border-2 rounded-full shadow-[2px_8px_4px_2px_rgba(0,0,0,1)]"
       />
-      <button className="px-8 py-2 text-2 bg-primary-light text-primary-white rounded-full shadow-[2px_6px_4px_0px_rgba(0,0,0,0.25)]">
-        입장
+      <button className="px-8 py-2 text-2 font-bold bg-primary-light text-primary-white rounded-full shadow-[2px_6px_4px_0px_rgba(0,0,0,0.25)]">
+        시작
       </button>
     </>
   );
